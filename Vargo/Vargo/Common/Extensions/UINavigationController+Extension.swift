90e02511660c0071f18a77612b77e71d2ddd4963
Andre Souza <andre.souza@mobicare.com.br>
2018-10-22 11:35:53 -0300

    - Organizando arquivos e diretórios para utilização de VIPER; - Configurando Cocoapods

---
+++ Vargo/Vargo/Common/Extensions/UINavigationController+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  UINavigationController+Extension.swift
+//  Vargo
+//
+//  Created by Andre Souza on 22/10/2018.
+//  Copyright © 2018 Andre Souza. All rights reserved.
+//
+
+import UIKit
+
+extension UINavigationController {
+    
+    func pushWireframe(_ wireframe: BaseWireframe, animated: Bool = true) {
+        self.pushViewController(wireframe.viewController, animated: animated)
+    }
+    
+    func setRootWireframe(_ wireframe: BaseWireframe, animated: Bool = true) {
+        self.setViewControllers([wireframe.viewController], animated: animated)
+    }
+    
+}
---
+++ Vargo/Vargo/Common/VIPER/BaseWireframe.swift
@@ -0,0 +1,87 @@
+//
+//  BaseWireframe.swift
+//  TheVargo
+//
+//  Created by Andre Souza on 06/07/2018.
+//  Copyright © 2018 AndreSamples. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+enum Transition {
+    case root
+    case push
+    case present
+}
+
+protocol WireframeInterface: class {
+    func popFromNavigationController(animated: Bool)
+    func dismiss(animated: Bool)
+    func show(_ wireframe: BaseWireframe, with transition: Transition, animated: Bool)
+    func show(_ viewController: UIViewController, with transition: Transition, animated: Bool)
+}
+
+class BaseWireframe {
+    
+    fileprivate unowned var _viewController: UIViewController
+    
+    init(viewController: UIViewController) {
+        _viewController = viewController
+    }
+    
+}
+
+extension BaseWireframe {
+    
+    var viewController: UIViewController {
+        return _viewController
+    }
+    
+    var navigationController: UINavigationController? {
+        return viewController.navigationController
+    }
+    
+}
+
+extension BaseWireframe: WireframeInterface {
+    
+    func popFromNavigationController(animated: Bool) {
+        let _ = navigationController?.popViewController(animated: animated)
+    }
+    
+    func dismiss(animated: Bool) {
+        navigationController?.dismiss(animated: animated)
+    }
+    
+    func show(_ wireframe: BaseWireframe, with transition: Transition, animated: Bool) {
+        switch transition {
+        case .push:
+            navigationController?.pushWireframe(wireframe)
+        case .present:
+            let navigationController = UINavigationController()
+            navigationController.setRootWireframe(wireframe)
+            _viewController.present(navigationController, animated: animated)
+        case .root:
+            navigationController?.setRootWireframe(wireframe, animated: animated)
+        }
+    }
+    
+    func show(_ viewController: UIViewController, with transition: Transition, animated: Bool) {
+        switch transition {
+        case .push:
+            navigationController?.pushViewController(viewController, animated: animated)
+        case .present:
+            _viewController.present(viewController, animated: animated)
+            if let popOver = viewController.popoverPresentationController {
+                popOver.sourceView = _viewController.view
+                if let rightBarbuttons = _viewController.navigationItem.rightBarButtonItems {
+                    popOver.barButtonItem = rightBarbuttons[rightBarbuttons.count - 1]
+                }
+            }
+        case .root:
+            navigationController?.setViewControllers([viewController], animated: animated)
+        }
+    }
+    
+}
